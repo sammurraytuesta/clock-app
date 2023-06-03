8d6a7bf27ab3b67ca93bcd2c3a6cfb22cbfba889
samantha murray <60449164+sammurraytuesta@users.noreply.github.com>
2023-06-02 22:46:00 -0700

    add pseudocode test cases

--- __tests__/ClockScreen.test.js
+++ __tests__/ClockScreen.test.js
@@ -5,3 +5,11 @@ import ClockScreen from '../src/screens/ClockScreen';
 test('renders correctly', async () => {
   render(<ClockScreen/>);
 }, 10000);
+
+//check for quote to populate element
+
+//check background image at a time (whether or not the correct image is showing for its time)
+
+//check correct background colors are rendering for time 
+
+//check new quote renders on refresh
