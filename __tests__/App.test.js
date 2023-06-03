ea402e540c29f5de37684047b54f94e4fce49d07
samantha murray <60449164+sammurraytuesta@users.noreply.github.com>
2023-06-02 22:37:49 -0700

    Add testing

---
+++ __tests__/App.test.js
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react-native';
+import App from '../App';
+
+// test if the app renders correctly without crashing
+test('renders correctly', async () => {
+  render(<App />);
+}, 10000);
